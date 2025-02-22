62dbe01a762f951c0ab23e828a58992671edf072
petrtimalev <petr.timalev@gmail.com>
2025-02-23 08:34:56 +1000

    update

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9dheb)s9ausofvvis7o5isk1ypb(+-y^yjj4_g*^=*yn@0-g53'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 
@@ -12,10 +11,14 @@ def categories(request, cat_id=None):
 
 
 def categories_by_slug(request, cat_slug):
-    if request.GET:
-        print(request.GET)
+    if request.POST:
+        print(request.POST)
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
 
 
 def archive(request, year):
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
